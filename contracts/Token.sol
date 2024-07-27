21b9574aa2e865d2cb7359fba1608ad60f0479b5
shottah <wasteofintel@gmail.com>
2024-07-26 23:18:36 -0400

    feat: downgrade solidity pragma 0.8.19

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.20;
+pragma solidity >=0.8.19;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
